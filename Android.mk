2234b179d919623984a8e3773bdfa1da08f9fc4f
TALU <taluemail272@gmail.com>
2021-08-23 19:05:10 +0100

    Add gtanotexlwifi as a possible target device
    Change-Id: I3a8540011169149aff88a88acf0a2ac9ad645041

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter gtaxlwifi,$(TARGET_DEVICE)),)
+ifneq ($(filter gtaxlwifi gtanotexlwifi,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libGLES_mali
