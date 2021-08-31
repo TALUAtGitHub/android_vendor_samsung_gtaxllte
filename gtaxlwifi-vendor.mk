b131eced371a0bc2cea6f402c19e8dd6e2876684
TALU <taluemail272@gmail.com>
2021-08-31 10:03:56 +0100

    gtaxl: import wcnss_filter from A520FZTU4BRB1
    This wcnss_filter binary is 64-bit, while the old one is only 32-bit.
    
    Change-Id: I036d540544f1dd486d318b249866201f212ef9b8

--- gtaxlwifi-vendor.mk
+++ gtaxlwifi-vendor.mk
@@ -133,7 +133,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtaxlwifi/proprietary/_audio/libLifevibes_lvverx.so:$(TARGET_COPY_OUT_VENDOR)/lib/libLifevibes_lvverx.so \
     vendor/samsung/gtaxlwifi/proprietary/_audio/libLifevibes_lvvetx.so:$(TARGET_COPY_OUT_VENDOR)/lib/libLifevibes_lvvetx.so
 
-# wcnss_filter - for Bluetooth - from the stock 7.1.1 firmware - T580XXU3BRF1
+# wcnss_filter - for Bluetooth - from A520FZTU4BRB1
 PRODUCT_COPY_FILES += \
     vendor/samsung/gtaxlwifi/proprietary/vendor/bin/wcnss_filter:$(TARGET_COPY_OUT_VENDOR)/bin/wcnss_filter
 
